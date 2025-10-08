3c145a090018ed0b51a9f5ae3a67fe4da64f3574
Seth <samaritanbrotherseth@gmail.com>
2025-10-08 16:41:50 -0400

    added content to Favorites and Home

--- final-app/src/pages/Favorites.jsx
+++ final-app/src/pages/Favorites.jsx
@@ -1,3 +1,17 @@
 export default function Favorites() {
-  return <h1>This is the Favorites page</h1>;
+  return (
+    <div className="favorites-header">
+      <header>
+        <nav>
+          <ul>
+            <li className="">Sad Movies</li>
+            <li className="">Excited Movies</li>
+            <li className="">Scared Movies</li>
+            <li className="">Excited Movies</li>
+            <li className="">Romantic Movies</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
--- final-app/src/pages/Home.jsx
+++ final-app/src/pages/Home.jsx
@@ -1,4 +1,9 @@
 import MoodSelector from "../features/moodSelector/MoodSelector.jsx";
+import styled from "styled-components";
+
+const Body = styled.body`
+  height: 100vh;
+`;
 
 export default function Home() {
   return (
